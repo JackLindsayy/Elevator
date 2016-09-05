1861179d29f0f75449bf60b487514e1bdb105f50
Guest User <Guest@stk-cs01-ws15.westlothian.sch.uk>
2016-09-05 10:47:05 +0100

    Finished occupants

--- Elevator.java
+++ Elevator.java
@@ -1,42 +1,55 @@
 
-
 public class Elevator
 {
     // instance variables - replace the example below with your own
     private int current_floor;
     private int top_floor;
     private int lowest_floor;
+    private int occupants;
+    private int max_occupants;
 
     public Elevator()
     {
         // initialise instance variables
         current_floor = 0;
         top_floor = 11;
+        occupants = 0;
+        max_occupants = 5;
     }
-    
+
     public void ascend() {
-       //tell the lift to go up
-       //add 1 on to the current_floor
-       //if current floor less than top_floor
-       if(current_floor < top_floor) {
-        current_floor = current_floor +1;
+        //tell the lift to go up
+        //add 1 on to the current_floor
+        //if current floor less than top_floor
+        if(current_floor < top_floor) {
+            current_floor = current_floor +1;
         }
         else {
             System.out.println("Sorry your at the top!");
         }
-       //else display error message
-     }
-     
+        //else display error message
+    }
+
     public void descend() {
-       //tell the lift to go up
-       //add 1 on to the current_floor
-       //if current floor less than top_floor
-       if(current_floor > lowest_floor) {
-        current_floor = current_floor -1;
+        //tell the lift to go up
+        //add 1 on to the current_floor
+        //if current floor less than top_floor
+        if(current_floor > lowest_floor) {
+            current_floor = current_floor -1;
         }
         else {
             System.out.println("Sorry your at the bottom!");
         }
-       //else display error message
-     }
+        //else display error message
+    }
+
+    public void board() {
+        if (occupants < max_occupants) {
+            occupants = occupants +1;    
+        }
+        else
+        {
+            System.out.println("Sorry the elevator is full!");
+        }
+    }
 }
